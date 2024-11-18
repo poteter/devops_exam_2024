84b0f2db09df2cd43390801286c3e26046b560b0
poteter <eith2312@gmail.com>
2024-11-18 20:42:26 +0000

    added cloudwatch alarm

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -14,15 +14,15 @@ resource "aws_sns_topic_subscription" "email_subscription" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "sqs_old_age_alarm" {
-  alarm_name                = "SQS_Oldest_Message_Age_Alarm"
-  comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = "2"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = "AWS/SQS"
-  period                    = "60"  
-  statistic                 = "Maximum"
-  threshold                 = "240"  
-  alarm_description         = "Alarm when the oldest message in SQS exceeds 4 minutes."
+  alarm_name          = "SQS_Oldest_Message_Age_Alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = "AWS/SQS"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "240"
+  alarm_description   = "Alarm when the oldest message in SQS exceeds 4 minutes."
   dimensions = {
     QueueName = aws_sqs_queue.image_queue.name
   }
@@ -31,5 +31,5 @@ resource "aws_cloudwatch_metric_alarm" "sqs_old_age_alarm" {
   ok_actions                = [aws_sns_topic.sqs_alarm_topic.arn]
   insufficient_data_actions = []
 
-  treat_missing_data        = "notBreaching"
+  treat_missing_data = "notBreaching"
 }
