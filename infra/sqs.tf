a4bf603568569f69df1cd469d3d76405ad86b15d
poteter <eith2312@gmail.com>
2024-11-18 20:30:27 +0000

    started implementing cloudwatch

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -2,3 +2,13 @@ resource "aws_sqs_queue" "image_queue" {
   name                       = var.sqs_queue_name
   visibility_timeout_seconds = 60
 }
+
+resource "aws_sns_topic" "sqs_alarm_topic" {
+  name = "sqs-alarm-notifications"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.sqs_alarm_topic.arn
+  protocol  = "email"
+  endpoint  = "thoeiv15@student.kristiania.no"
+}
