c4f7be307f6268bfe79903b6eb50853359b4903e
poteter <eith2312@gmail.com>
2024-11-18 20:40:30 +0000

    added cloudwatch alarm

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -12,3 +12,24 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol  = "email"
   endpoint  = "thoeiv15@student.kristiania.no"
 }
+
+resource "aws_cloudwatch_metric_alarm" "sqs_old_age_alarm" {
+  alarm_name                = "SQS_Oldest_Message_Age_Alarm"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "ApproximateAgeOfOldestMessage"
+  namespace                 = "AWS/SQS"
+  period                    = "60"  
+  statistic                 = "Maximum"
+  threshold                 = "240"  
+  alarm_description         = "Alarm when the oldest message in SQS exceeds 4 minutes."
+  dimensions = {
+    QueueName = aws_sqs_queue.image_queue.name
+  }
+
+  alarm_actions             = [aws_sns_topic.sqs_alarm_topic.arn]
+  ok_actions                = [aws_sns_topic.sqs_alarm_topic.arn]
+  insufficient_data_actions = []
+
+  treat_missing_data        = "notBreaching"
+}
